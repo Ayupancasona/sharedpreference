53c8c63d6efbada4072c38bb9e4c1a086128bb58
Ayupancasona <60374898+Ayupancasona@users.noreply.github.com>
2020-03-14 00:26:27 +0700

    test

---
+++ app/src/main/java/com/example/sharedpreferenceloginlogout/dashboard.kt
@@ -0,0 +1,33 @@
+package com.example.sharedpreferenceloginlogout
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+class dashboard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        button.setOnClickListener {
+            val sharedPreferences = getSharedPreferences("CEKLOGIN", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+
+            editor.putString("STATUS", "0")
+            editor.apply()
+
+            startActivity(Intent(this@dashboard, MainActivity::class.java))
+            finish()
+        }
+
+    }
+}
+kata wardah yg terakhir itu bikin db mahasiswa
+        isinya ada tabel user, tabel data mahasiswa
+        user -> username, password
+        data mahasiswa -> tapi dia blang id nama nomor mungkin nim alamat
+
+        aku kalo upload ke github  mesti eror
